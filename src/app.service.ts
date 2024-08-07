70dd7a05d26323acef853008a4a7113fbfe8eb0a
Shubham More <mores9785@gmail.com>
2024-08-07 16:39:18 +0530

    chore: Refactor app controller and service to return product items

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getProductItems(): Promise<Products.Item[]> {
+    return [
+      {
+        'id': 1,
+        'name': 'Product 1',
+        'description': 'Product 1 description',
+        'price': 10,
+        'image': 'https://via.placeholder.com/150'
+      },
+      {
+        'id': 2,
+        'name': 'Product 2',
+        'description': 'Product 2 description',
+        'price': 20,
+        'image': 'https://via.placeholder.com/150'
+      }
+    ]
   }
 }
